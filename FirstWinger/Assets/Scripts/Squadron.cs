b4200d382d41b44ec4c3b23cd6bd19a86479fa44
94mark <90877724+94mark@users.noreply.github.com>
2022-02-12 20:47:23 +0900

    Update Squadron

--- FirstWinger/Assets/Scripts/Squadron.cs
+++ FirstWinger/Assets/Scripts/Squadron.cs
@@ -2,8 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
+public class EnemyGenerateData
+{
+    public string FilePath;
+    public int MaxHP;
+    public int Damage;
+    public int CrashDamage;
+    public float BulletSpeed;
+    public int FireRemainCount;
+    public int GamePoint;
+
+    public Vector3 GeneratePoint; //입장 전 생성 위치
+    public Vector3 AppearPoint; //입장시 도착 위치
+
+    public Vector3 DisappearPoint; //퇴장 시 목표 위치
+}
+
 public class Squadron : MonoBehaviour
 {
+    [SerializeField]
+    EnemyGenerateData[] enemyGenerateDatas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +34,17 @@ public class Squadron : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            GenerateAllData();
+        }
+    }
+
+    void GenerateAllData()
+    {
+        for(int i = 0; i < enemyGenerateDatas.Length; i++)
+        {
+            SystemManager.Instance.EnemyManager.GenerateEnemy(enemyGenerateDatas[i].FilePath, enemyGenerateDatas[i].GeneratePoint);
+        }
     }
 }
