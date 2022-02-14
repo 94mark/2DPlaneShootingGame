64267686ce4401657b816cd020d755c01f7f65f4
94mark <90877724+94mark@users.noreply.github.com>
2022-02-14 22:12:56 +0900

    Update BaseSceneMain.cs

---
+++ FirstWinger/Assets/Scripts/BaseSceneMain.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSceneMain : MonoBehaviour
+{
+    private void Awake()
+    {
+        OnAwake();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnStart();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScene();
+    }
+
+    void OnDestroy()
+    {
+        Terminate();
+    }
+
+    protected virtual void OnAwake()
+    {
+
+    }
+
+    protected virtual void OnStart()
+    {
+
+    }
+
+    public virtual void Initialize()
+    {
+
+    }
+
+    protected virtual void UpdateScene()
+    {
+
+    }
+
+    protected virtual void Terminate()
+    {
+
+    }
+}
